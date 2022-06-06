a1c65e865cb0b9a4cfac53cdf8b827d050372057
dharmik <dharmikvaishnav0419@gmail.com>
2022-06-07 00:01:41 +0530

    second commit

---
+++ src/App.js
@@ -0,0 +1,81 @@
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell, { tableCellClasses } from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import { useState,useEffect } from 'react';
+
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+  [`&.${tableCellClasses.head}`]: {
+    backgroundColor: theme.palette.common.black,
+    color: theme.palette.common.white,
+  },
+  [`&.${tableCellClasses.body}`]: {
+    fontSize: 14,
+  },
+}));
+
+const StyledTableRow = styled(TableRow)(({ theme }) => ({
+  '&:nth-of-type(odd)': {
+    backgroundColor: theme.palette.action.hover,
+  },
+  // hide last border
+  '&:last-child td, &:last-child th': {
+    border: 0,
+  },
+}));
+
+
+
+export default function ListTable() {
+  const [rows,setRows] = useState([])
+
+  localStorage.setItem("data",JSON.stringify([{"id":1,"name":"shakti","email":"shakti@gmail,com"},{"id":1,"name":"shakti","email":"shakti@gmail,com"}]))
+
+  const getData = () =>{
+    const data = JSON.parse(localStorage.getItem("data"))
+    if(data){
+      setRows(data)
+    }else{
+      localStorage.setItem("data",[])
+    }
+  }
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+
+
+
+  return (
+    <TableContainer component={Paper} sx={{ width: 900,mt:2,m:"auto" }} >
+      <Table sx={{ minWidth: 700 }} aria-label="customized table">
+        <TableHead>
+          <TableRow>
+            <StyledTableCell>Id</StyledTableCell>
+            <StyledTableCell align="right">Name</StyledTableCell>
+            <StyledTableCell align="right">Email</StyledTableCell>
+            <StyledTableCell align="right">Password</StyledTableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <StyledTableRow key={row.name}>
+              <StyledTableCell component="th" scope="row">
+                {row.id}
+              </StyledTableCell>
+              <StyledTableCell align="right">{row.name}</StyledTableCell>
+              <StyledTableCell align="right">{row.email}</StyledTableCell>
+              <StyledTableCell align="right">{row.password}</StyledTableCell>
+            </StyledTableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
---
+++ src/comp/ListTable.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ListTable = () => {
+  return (
+    <div>ListTable</div>
+  )
+}
+
+export default ListTable
